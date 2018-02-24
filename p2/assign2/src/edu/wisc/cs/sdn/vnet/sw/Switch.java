609a042a5f15010b363ea605807d6a3a8e85c48e
leewaymay <leewaymay@gmail.com>
2018-02-24 16:44:02 -0600

    part 2

--- p2/assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ p2/assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -4,19 +4,28 @@ import net.floodlightcontroller.packet.Ethernet;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+import net.floodlightcontroller.packet.MACAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
-{	
+{
+	/** switch table **/
+	private Map<MACAddress, SwitchEntry> switchTable;
+
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
 	 */
 	public Switch(String host, DumpFile logfile)
 	{
+
 		super(host,logfile);
+		switchTable = new HashMap<>();
 	}
 
 	/**
@@ -28,10 +37,29 @@ public class Switch extends Device
 	{
 		System.out.println("*** -> Received packet: " +
                 etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		/********************************************************************/
+
+		MACAddress hostMACAddress = etherPacket.getSourceMAC();
+		MACAddress destMACAddress = etherPacket.getDestinationMAC();
+
+		SwitchEntry newEntry = new SwitchEntry(hostMACAddress, inIface);
+		switchTable.put(hostMACAddress, newEntry);
+		SwitchEntry retrieveEntry = switchTable.get(destMACAddress);
+		if (retrieveEntry != null && retrieveEntry.isValid()) {
+			if (retrieveEntry.getIface().getName().equals(inIface.getName())) {
+				// drop the frame, do nothing
+			} else {
+				sendPacket(etherPacket, retrieveEntry.getIface())
+			}
+		} else {
+			for (Map.Entry<String, Iface> e : interfaces.entrySet()) {
+				if (!e.getKey().equals(inIface.getName())) {
+					sendPacket(etherPacket, e.getValue());
+				}
+			}
+		}
+		// clean expired switch entry
+		if (retrieveEntry != null && !retrieveEntry.isValid()) {
+			switchTable.remove(destMACAddress);
+		}
 	}
 }
