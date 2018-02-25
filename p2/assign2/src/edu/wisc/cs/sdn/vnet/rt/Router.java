f0c2e454449ea1d55df19bdacca362e944e1aa33
leewaymay <leewaymay@gmail.com>
2018-02-24 20:17:15 -0600

    part 3

--- p2/assign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ p2/assign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -5,6 +5,10 @@ import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
 
 /**
  * @author Aaron Gember-Jacobson and Anubhavnidhi Abhashkumar
@@ -16,6 +20,7 @@ public class Router extends Device
 	
 	/** ARP cache for the router */
 	private ArpCache arpCache;
+
 	
 	/**
 	 * Creates a router for a specific host.
@@ -81,11 +86,64 @@ public class Router extends Device
 	{
 		System.out.println("*** -> Received packet: " +
                 etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		
-		/********************************************************************/
+		// check packets
+		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4) {
+			// drop the packet
+			return;
+		}
+		IPv4 p = (IPv4) etherPacket.getPayload();
+		// checksum
+		short oldChecksum = p.getChecksum();
+		p.resetChecksum();
+
+		byte[] data = new byte[p.getHeaderLength()*4];
+		ByteBuffer bb = ByteBuffer.wrap(data);
+
+		bb.put((byte) (((p.getVersion() & 0xf) << 4) | (p.getHeaderLength() & 0xf)));
+		bb.put(p.getDiffServ());
+		bb.putShort(p.getTotalLength());
+		bb.putShort(p.getIdentification());
+		bb.putShort((short) (((p.getFlags() & 0x7) << 13) | (p.getFragmentOffset() & 0x1fff)));
+		bb.put(p.getTtl());
+		bb.put(p.getProtocol());
+		bb.putShort(p.getChecksum());
+		bb.putInt(p.getSourceAddress());
+		bb.putInt(p.getDestinationAddress());
+		if (p.getOptions() != null)
+			bb.put(p.getOptions());
+
+		bb.rewind();
+		int accumulation = 0;
+		for (int i = 0; i < p.getHeaderLength() * 2; ++i) {
+			accumulation += 0xffff & bb.getShort();
+		}
+		accumulation = ((accumulation >> 16) & 0xffff)
+				+ (accumulation & 0xffff);
+		short newChecksum = (short) (~accumulation & 0xffff);
+
+		if (oldChecksum != newChecksum) {
+			return;
+		}
+		p.setChecksum(newChecksum);
+		// TTL
+		int ttl = p.getTtl() - 1;
+		if (ttl == 0) {
+			return;
+		}
+		// Decide the interface
+		int destIp = p.getDestinationAddress();
+		for (Map.Entry<String, Iface> e : this.interfaces.entrySet()) {
+			if (e.getValue().getIpAddress() == destIp) {
+				return;
+			}
+		}
+		RouteEntry e = routeTable.lookup(destIp);
+		if (e != null) {
+			int nextIp = e.getGatewayAddress();
+			ArpEntry arpEntry = arpCache.lookup(nextIp);
+			etherPacket.setDestinationMACAddress(arpEntry.getMac().toString());
+			etherPacket.setSourceMACAddress(e.getInterface().getMacAddress().toString());
+			sendPacket(etherPacket, e.getInterface());
+		}
 	}
 }
