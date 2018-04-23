c2b7ce93ebb549c9dc6195a9957b17af954b41c7
leewaymay <leewaymay@gmail.com>
2018-04-23 17:10:33 -0500

    initial framework

--- p4/assign2_soln/TCPReceiverThread.java
+++ p4/assign2_soln/TCPReceiverThread.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class TCPReceiverThread extends Thread {
 
 	protected DatagramSocket socket = null;
-	protected BufferedReader out = null;
+	protected PrintWriter out = null;
 	protected boolean moreQuotes = true;
 
 	int port;
@@ -16,7 +16,12 @@ public class TCPReceiverThread extends Thread {
 	}
 
 	public TCPReceiverThread(int port, int mtu, int sws){
-		socket = new DatagramSocket(port);
+		try{
+			socket = new DatagramSocket(port);
+		} catch (Exception e) {
+			//do nothing
+		}
+
 		this.port = port;
 		this.mtu = mtu;
 		this.sws = sws;
@@ -29,48 +34,41 @@ public class TCPReceiverThread extends Thread {
 	}
 
 	public void run() {
-
-		while (moreQuotes) {
-			try {
-				byte[] buf = new byte[256];
-
-				// receive request
-				DatagramPacket packet = new DatagramPacket(buf, buf.length);
-				socket.receive(packet);
-
-				// figure out response
-				String dString = null;
-				if (in == null)
-					dString = new Date().toString();
-				else
-					dString = getNextQuote();
-
-				buf = dString.getBytes();
-
-				// send the response to the client at "address" and "port"
-				InetAddress address = packet.getAddress();
-				int port = packet.getPort();
-				packet = new DatagramPacket(buf, buf.length, address, port);
-				socket.send(packet);
-			} catch (IOException e) {
-				e.printStackTrace();
-				moreQuotes = false;
-			}
+		String hostname = "10.0.1.101";
+		// get a datagram socket
+		try{
+			socket = new DatagramSocket(port);
+		} catch (Exception e) {
+			//do nothing
 		}
-		socket.close();
-	}
-
-	protected String getNextQuote() {
-		String returnValue = null;
+		// send request
+		byte[] buf = new byte[256];
+		InetAddress address = null;
 		try {
-			if ((returnValue = in.readLine()) == null) {
-				in.close();
-				moreQuotes = false;
-				returnValue = "No more quotes. Goodbye.";
-			}
-		} catch (IOException e) {
-			returnValue = "IOException occurred in server.";
+			address = InetAddress.getByName(hostname);
+			System.out.println("The host address is " + address.toString());
+		} catch (Exception e) {
+			// do nothing
 		}
-		return returnValue;
+		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			// do nothing
+		}
+
+		// get response
+		packet = new DatagramPacket(buf, buf.length);
+		try {
+			socket.receive(packet);
+		} catch (IOException e){
+			// do nothing
+		}
+
+		// display response
+		String received = new String(packet.getData(), 0, packet.getLength());
+		System.out.println("Quote of the Moment: " + received);
+
+		socket.close();
 	}
 }
